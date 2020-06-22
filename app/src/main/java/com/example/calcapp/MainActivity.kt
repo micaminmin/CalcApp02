c6de5d44afa6a0d42f97cf720c55efe3391f60f4
noriyuki mikami <ysfplem@yahoo.co.jp>
2020-06-22 16:40:37 +0900

    再挑戦

--- app/src/main/java/com/example/calcapp/MainActivity.kt
+++ app/src/main/java/com/example/calcapp/MainActivity.kt
@@ -5,6 +5,8 @@ import android.os.Bundle
 import android.view.View
 import kotlinx.android.synthetic.main.activity_main.*
 import android.content.Intent
+import java.lang.Exception
+import android.support.design.widget.Snackbar
 
 class MainActivity : AppCompatActivity(), View.OnClickListener {
 
@@ -16,31 +18,42 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
         button2.setOnClickListener(this)
         button3.setOnClickListener(this)
         button4.setOnClickListener(this)
+
+        Snackbar.make(view, "数字を入力して下さい", Snackbar.LENGTH_INDEFINITE)
+            .setAction("Back"){
+        }.show()
     }
 
     override fun onClick(v: View) {
         val intent = Intent(this, ResultActivity::class.java)
-        intent.putExtra("Result", 0)
-        startActivity(intent)
 
-        var str1:String = editTexe1.text.toString()
-        var str2:String = editTexe2.text.toString()
+        try {
 
-        var num1:Double = str1.toDouble()
-        var num2:Double = str2.toDouble()
-        var result = 0.0
+            var str1: String = editText1.text.toString()
+            var str2: String = editText2.text.toString()
 
-        if (v.id == R.id.button1) {
-            result = num1 + num2
+            var num1: Double = str1.toDouble()
+            var num2: Double = str2.toDouble()
+            var result = 0.0
 
-        }else if (v.id == R.id.button2) {
-            result = num1 - num2
+            if (v.id == R.id.button1) {
+                result = num1 + num2
 
-        }else if (v.id == R.id.button3) {
-            result = num1 * num2
+            } else if (v.id == R.id.button2) {
+                result = num1 - num2
 
-        }else if (v.id == R.id.button4) {
-            result = num1 / num2
+            } else if (v.id == R.id.button3) {
+                result = num1 * num2
+
+            } else if (v.id == R.id.button4) {
+                result = num1 / num2
+            }
+
+            intent.putExtra("Result", result)
+            startActivity(intent)
+
+        } catch (e: Exception) {
+            .text = ("数字を入力して下さい")
         }
     }
 }
